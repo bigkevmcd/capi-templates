b7cb9ec4fb2160cae413881d92219d2796b5e4e8
Kevin McDermott <bigkevmcd@gmail.com>
2021-05-19 18:16:37 +0100

    Multidoc metadata support
    This adds support for finding a Metadata object within a Multidoc
    format.

--- pkg/flavours/multidoc.go
+++ pkg/flavours/multidoc.go
@@ -7,10 +7,22 @@ import (
 	"sort"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
 	processor "sigs.k8s.io/cluster-api/cmd/clusterctl/client/yamlprocessor"
 	sigsyaml "sigs.k8s.io/yaml"
 )
 
+var (
+	serializer  = yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+	metadataGVK = &schema.GroupVersionKind{Group: "capi.weave.works", Version: "templates", Kind: "TemplateMetadata"}
+)
+
+// ParseMultiDoc takes a single template with multiple YAML documents, and
+// parses the parameters from it.
+//
+// If a TemplateMetadata object is found, the parameters returned are enriched
+// with the data.
 func ParseMultiDoc(fname string) ([]Param, error) {
 	b, err := os.ReadFile(fname)
 	if err != nil {
@@ -27,12 +39,12 @@ func ParseMultiDoc(fname string) ([]Param, error) {
 		var data unstructured.Unstructured
 		_, gvk, err := serializer.Decode(v, nil, &data)
 		if err != nil {
-			return nil, fmt.Errorf("failed to unmarshal to unstructured %s: %w", v, err)
+			return nil, fmt.Errorf("failed to unmarshal %s to unstructured %s: %w", fname, v, err)
 		}
 		if gvk.String() == metadataGVK.String() {
 			m, err := parseMetadata(v)
 			if err != nil {
-				return nil, fmt.Errorf("failed to parse metadata from TemplateMetadata: %w", err)
+				return nil, fmt.Errorf("failed to parse metadata from TemplateMetadata in file %s: %w", fname, err)
 			}
 			if m != nil {
 				metas = m
@@ -40,7 +52,7 @@ func ParseMultiDoc(fname string) ([]Param, error) {
 		}
 		tv, err := proc.GetVariables(v)
 		if err != nil {
-			return nil, fmt.Errorf("processing template: %w", err)
+			return nil, fmt.Errorf("processing template in file %s: %w", fname, err)
 		}
 		for _, n := range tv {
 			variables[n] = true
--- pkg/flavours/multidoc_test.go
+++ pkg/flavours/multidoc_test.go
@@ -5,27 +5,44 @@ import (
 
 	"github.com/bigkevmcd/capi-templates/test"
 	"github.com/google/go-cmp/cmp"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
 )
 
-var (
-	serializer  = yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
-	metadataGVK = &schema.GroupVersionKind{Group: "capi.weave.works", Version: "templates", Kind: "TemplateMetadata"}
-)
-
-func TestParseMultiDoc_with_invalid_yaml(t *testing.T) {
-	t.Skip()
-}
-
-func TestParseMultiDoc_with_unknown_file(t *testing.T) {
-	_, err := ParseMultiDoc("testdata/unknownyaml")
-	test.AssertErrorMatch(t, "failed to read template", err)
+func TestParseMultiDoc_errors(t *testing.T) {
+	parseTests := []struct {
+		filename string
+		errMsg   string
+	}{
+		{"testdata/bad_multidoc.yaml", "processing template.*bad substitution"},
+		{"testdata/bad_multidoc_corrupt_metadata.yaml", "failed to unmarshal testdata/bad_multidoc_corrupt_metadata.yaml to unstructured"},
+		{"testdata/bad_multidoc_invalid_metadata.yaml", "failed to parse metadata from TemplateMetadata"},
+		{"testdata/unknownyaml", "failed to read template"},
+	}
+	for _, tt := range parseTests {
+		t.Run(tt.filename, func(t *testing.T) {
+			_, err := ParseMultiDoc(tt.filename)
+			test.AssertErrorMatch(t, tt.errMsg, err)
+		})
+	}
 }
 
 func TestParseMultiDoc_with_no_metadata(t *testing.T) {
-	t.Skip()
+	params, err := ParseMultiDoc("testdata/multidoc_with_no_metadata.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := []Param{
+		{Name: "AWS_CONTROL_PLANE_MACHINE_TYPE"},
+		{Name: "AWS_NODE_MACHINE_TYPE"},
+		{Name: "AWS_REGION"},
+		{Name: "AWS_SSH_KEY_NAME"},
+		{Name: "CLUSTER_NAME"},
+		{Name: "CONTROL_PLANE_MACHINE_COUNT"},
+		{Name: "KUBERNETES_VERSION"},
+		{Name: "WORKER_MACHINE_COUNT"},
+	}
+	if diff := cmp.Diff(want, params); diff != "" {
+		t.Fatalf("parsing failed:\n%s", diff)
+	}
 }
 
 func TestParseMultiDoc(t *testing.T) {
